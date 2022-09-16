0286cc74dcb3244f5a20bcebf6fce47af494c5dd
talialino <talia-apiu@hotmail.com>
2022-09-16 01:13:05 -0300

    second commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text, ScrollView} from 'react-native';
+
+// import { Container } from './styles';
+
+const App = () => {
+  return (
+    <View>
+      <ScrollView>
+        <Text>Hello world</Text>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default App;
